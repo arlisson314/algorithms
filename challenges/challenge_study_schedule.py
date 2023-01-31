2e89d379f1ada980714fa7ca9d0500928e346562
Arlisson N. Costa <arlisson314@gmail.com>
2023-01-31 10:40:32 -0300

    feat:  implementa Algoritmo que busca número de estudantes estudando no mesmo horário

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,12 @@
 def study_schedule(permanence_period, target_time):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    count = 0
+
+    if not target_time or target_time < 0:
+        return None
+
+    for i in permanence_period:
+        if not isinstance(i[0], int) or not isinstance(i[1], int):
+            return None
+        if target_time in range(i[0], i[1] + 1):
+            count += 1
+    return count

dbbde7dea279d19b7525f80c73bc319af16922f0
Arlisson N. Costa <arlisson314@gmail.com>
2023-01-31 13:52:38 -0300

    test: cria testes para função encrypt_message

--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -1,5 +1,19 @@
-# from challenges.challenge_encrypt_message import encrypt_message
+from challenges.challenge_encrypt_message import encrypt_message
+import pytest
 
 
 def test_encrypt_message():
-    pass
+    """verifca se a funccão encrypt_message retorna erro quando
+    o tipo de key e message são diferentes de int e str"""
+    with pytest.raises(TypeError):
+        encrypt_message(1, "string") or encrypt_message("string", "string")
+
+    """verifica se retornar o valor esperado quando key é impar"""
+    assert encrypt_message("xablau", 3) == "bax_ual"
+
+    """verifica se retornar o valor esperado quando key é par"""
+    assert encrypt_message("xablau", 2) == "ualb_ax"
+
+    """verifica se retornar o valor esperado quando key não é
+    um index válido"""
+    assert encrypt_message("xablau", 6) == "ualbax"

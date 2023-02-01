4cc368c13da43fc7282509143b62f0927496b1e3
Arlisson N. Costa <arlisson314@gmail.com>
2023-02-01 14:17:39 -0300

    feat: executa challenge_find_the_duplicate que Encontrando números repetidos

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,3 +1,17 @@
+from challenges.challenge_anagrams import merge_sort
+
+
 def find_duplicate(nums):
-    """Faça o código aqui."""
-    raise NotImplementedError
+
+    merge_sort(nums)
+
+    for i in range(len(nums) - 1):
+
+        if not nums or not isinstance(nums[i], int):
+            return False
+        if nums[i] < 0:
+            return False
+
+        if nums[i] == nums[i + 1]:
+            return nums[i]
+    return False

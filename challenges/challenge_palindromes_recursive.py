ee8426029d4abf6a04e6e8a5c304237e93c8e97b
Arlisson N. Costa <arlisson314@gmail.com>
2023-03-07 19:54:50 -0300

    doc: adiciona comentario sobre o nivel de complexidade da função is_palindrome_recursive

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,3 +1,12 @@
+"""O nível de complexidade desse código é O(n), onde n é o comprimento da
+palavra a ser verificada. Isso porque a função é recursiva e realiza a
+verificação comparando o primeiro caractere com o último, o segundo caractere
+com o penúltimo, e assim por diante, até que os índices de início e fim se
+encontrem ou não sejam iguais. Em cada iteração, o comprimento da string a
+ser verificada é dividido aproximadamente pela metade, o que significa que
+o número de comparações necessárias é proporcional ao tamanho da string."""
+
+
 def is_palindrome_recursive(word, low_index, high_index):
     if not word or word[low_index] != word[high_index]:
         return False
